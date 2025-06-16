0804796aa6be446295df5647dfbd1cabb2f8446d
Raymond Akiiki <raymondakiikikirungi@gmail.com>
2025-06-16 11:51:23 +0300

    fix: update Vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'http://localhost:5000', // Ensure this points to the correct base
+  base: './', // Ensure this points to the correct base
   server: {
     open: true,
     // Configure server to return index.html for unknown routes
